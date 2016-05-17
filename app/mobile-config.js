00ee91bf6c5252501c81e13ebcd1666d743fe735
CadeBrown <brown.cade@gmail.com>
2016-05-16 21:33:14 -0400

    Updated

--- app/mobile-config.js
+++ app/mobile-config.js
@@ -9,27 +9,41 @@ App.info({
 
 // Set up resources such as icons and launch screens.
 App.icons({
-  'iphone': 'icons/icon-60.png',
-  'iphone_2x': 'icons/icon-60@2x.png',
+  //Apple
+  'iphone': 'resources/res/icons/ios/icon-60.png',
+  'iphone_2x': 'resources/res/icons/ios/icon-60-2x.png',
+  'iphone_3x': 'resources/res/icons/ios/icon-60-3x.png',
+  'ipad': 'resources/res/icons/ios/icon-76.png',
+  'ipad_2x': 'resources/res/icons/ios/icon-76-2x.png',
+  'ios_settings': 'resources/res/icons/ios/icon-small.png',
+  'ios_settings_2x': 'resources/res/icons/ios/icon-small-2x.png',
+  'ios_spotlight': 'resources/res/icons/ios/icon-40.png',
+  'ios_spotlight_2x': 'resources/res/icons/ios/icon-40-2x.png',
+  //You have to custom make ipad pro logo :(
+  //'ipad_pro': 'resources/res/icons/ios/icon-76-2x.png',
+  //Android
   'android_ldpi': 'resources/res/icons/android/icon-36-ldpi.png',
   'android_mdpi': 'resources/res/icons/android/icon-48-mdpi.png',
   'android_hdpi': 'resources/res/icons/android/icon-72-hdpi.png',
-  'android_xhdpi': 'resources/res/icons/android/icon-96-xhdpi',
-  // ... more screen sizes and platforms ...
+  'android_xhdpi': 'resources/res/icons/android/icon-96-xhdpi.png',
+  'android_xxhdpi': 'resources/res/icons/android/icon-144-xxhdpi.png',
+  'android_xxxhdpi': 'resources/res/icons/android/icon-192-xxxhdpi.png',
 });
 
 App.launchScreens({
-  'iphone': 'splash/Default~iphone.png',
-  'iphone_2x': 'splash/Default@2x~iphone.png',
-  // ... more screen sizes and platforms ...
+  //Apple
+  'iphone': 'resources/res/screens/ios/screen-iphone-portrait.png',
+  'iphone_2x': 'resources/res/screens/ios/screen-iphone-portrait-2x.png',
+  'iphone5': 'resources/res/screens/ios/screen-iphone-portrait-568h-2x.png',
+  
+  //Android
+  'android_mdpi_portrait': 'resources/res/screens/android/screen-mdpi-portrait.png',
+  'android_hdpi_portrait': 'resources/res/screens/android/screen-hdpi-portrait.png',
+  'android_xhdpi_portrait': 'resources/res/screens/android/screen-xhdpi-portrait.png',
 });
 
 // Set PhoneGap/Cordova preferences
-App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('BackgroundColor', '0x20001c9f');
 App.setPreference('HideKeyboardFormAccessoryBar', true);
 
-// Pass preferences for a particular PhoneGap/Cordova plugin
-App.configurePlugin('com.phonegap.plugins.facebookconnect', {
-  APP_ID: '1234567890',
-  API_KEY: 'supersecretapikey'
-});
+
